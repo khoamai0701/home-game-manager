573568a27b4d5364ec257b9536e33a6baf45f58b
Khoa Mai <khoamai0701@gmail.com>
2026-09-12 15:00:44 -0700

    add group stats route

--- server/routes/stats.js
+++ server/routes/stats.js
@@ -18,6 +18,24 @@ router.get('/', async (req, res) => {
 
 })
 
+router.get('/:group_id', async (req, res) => {
+    const groupId = req.params.group_id
+    const groupStats = await pool.query(`
+        SELECT 
+            users.display_name, 
+            SUM(amount) FILTER (WHERE type != 'cashout' AND status = 'approved') AS total_buy_in,
+            SUM(amount) FILTER (WHERE type = 'cashout' AND status = 'approved') AS total_cash_out
+        FROM transactions
+        JOIN players ON transactions.player_id = players.id
+        JOIN group_members ON players.user_id = group_members.user_id 
+        JOIN users ON group_members.user_id = users.id
+        WHERE group_members.group_id = $1
+        GROUP BY users.display_name`, [groupId])
+    res.json(groupStats.rows)
+        
+
+})
+
 
 
 export default router
